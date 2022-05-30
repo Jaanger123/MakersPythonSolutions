4404d49baa654baf27bc73e4869a14b3acd2d09e
Aigerim <maviboncuaika@gmail.com>
2022-05-30 18:18:12 +0600

    task2

---
+++ 9. Scopes/Task 1/Task2/solution1.py
@@ -0,0 +1,9 @@
+x = 'Я глобальная переменная!'
+print(x)
+
+def my_func():
+    global x
+    x = 'Я могу изменяться'
+    
+my_func()
+print(x)
--- 9. Scopes/Task 1/solution1.py
+++ 9. Scopes/Task 1/solution1.py
@@ -1,10 +1,1 @@
 # solution1
-x = 'Я глобальная переменная!'
-print(x)
-
-def my_func():
-    global x
-    x = 'Я могу изменяться'
-    
-my_func()
-print(x)
