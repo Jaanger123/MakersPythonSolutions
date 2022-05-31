dc832a4655b5316cbb758138f20d6a2b067bd3bc
Uluk <nelson.mandela.99.99@gmail.com>
2022-05-31 20:48:05 +0600

    10task

--- 12. OOP/task10/solution1.py
+++ 12. OOP/task10/solution1.py
@@ -21,32 +21,3 @@ prior3 = ToDo('выгулять собаку')
 prior2.give_priority(3)
 print(ToDo.instances)
 print(prior1.list_of_tasks())
-
-class Spam:
-    val = 1
-
-    def __init__(self, n):
-        val = 5
-
-obj = Spam(5)
-print(obj.val)
-
-class Nums:
-    def __init__(self, num):
-        self.num = num
-        print(f'1000 - {self.num}')
-
-Nums('7')
-
-
-class A:
-    def __init__(self) -> None:
-        self.a = 0
-
-    def change(self,n):
-
-        a = n
-
-obj = A()
-obj.change(2)
-print(obj.a)
