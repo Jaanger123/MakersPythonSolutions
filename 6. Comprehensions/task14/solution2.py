491e0e760132e6c820db32121719ddb6e10e90b6
Uluk <nelson.mandela.99.99@gmail.com>
2022-06-02 11:51:00 +0600

    fixed 14task 2solution

--- 6. Comprehensions/task14/solution2.py
+++ 6. Comprehensions/task14/solution2.py
@@ -4,5 +4,5 @@ dict_ = {'Timur': {'history': 90, 'math': 95, 'literature': 91},
  
  'Nik': {'history': 84, 'math': 85, 'literature': 87}}
 
-dict2 = {k : [k2 for k2, v2 in v.items() if v2 == max(v.values())][0] for k, v in dict_.items() }
-print(dict2)
+new_dict = {k : [k2 for k2, v2 in v.items() if v2 == max(v.values())][0] for k, v in dict_.items() }
+print(new_dict)
