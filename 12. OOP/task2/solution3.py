da5b72fec62705b27aab4113e48b3cd977659a46
Uluk <nelson.mandela.99.99@gmail.com>
2022-06-02 11:42:59 +0600

    hello

---
+++ 12. OOP/task2/solution3.py
@@ -0,0 +1,14 @@
+import math
+class Circle:
+    color = 'Синий'
+    pi = 3.14
+    def __init__(self, radius):
+        self.radius = radius
+
+    def get_area(self):
+        return self.pi * pow(self.radius,2)
+
+circle = Circle(2)
+circle.color = 'Красный'
+print(circle.color)
+print(circle.get_area())
