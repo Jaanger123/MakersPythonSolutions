52d968aa773fc11cbf53fc9ff5e062b7323b890f
Aigerim <maviboncuaika@gmail.com>
2022-06-05 20:37:21 +0600

    ..

--- 9. Scopes/Task 1/solution1.py
+++ 9. Scopes/Task 1/solution1.py
@@ -9,10 +9,4 @@ def foo():
 
     bar()
 foo()
-<<<<<<< HEAD
 print("переменная в foo: ", var)
-
-
-=======
-print("переменная в foo: ", var)
->>>>>>> 96d85f84477fbda7b8c52fb371d9ebc3c73ef002
--- 9. Scopes/Task2/solution1.py
+++ 9. Scopes/Task2/solution1.py
@@ -6,9 +6,5 @@ def my_func():
     x = 'Я могу изменяться'
     
 my_func()
-<<<<<<< HEAD
-
-=======
->>>>>>> 96d85f84477fbda7b8c52fb371d9ebc3c73ef002
 print(x)
 print(globals())
